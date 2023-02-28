94579dbf27e441ee3381464eb3bebcf38af38fb7
Ross Lew <rgglewerenz@gmaill.com>
2023-02-27 21:29:57 -0500

    Created a starting point for the task form

--- DTO/TaskTransferModal.cs
+++ DTO/TaskTransferModal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace DTO
 {
     public class TaskTransferModal
     {
+        
         public int TaskID { get; set; }
-        public string Title { get; set; } = "";
-        public string Description { get; set; } = "";
+		[Required]
+		[StringLength(50, ErrorMessage = "Title must be less than 50 characters.")]
+
+		public string Title { get; set; } = "";
+		[Required]
+		[StringLength(150, ErrorMessage = "Description must be less than 150 characters")]
+		public string Description { get; set; } = "";
         public TaskRecurrenceTransferModal? recurrenceOptions { get; set; }
     }
 }
