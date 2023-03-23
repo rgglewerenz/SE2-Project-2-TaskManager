6fda65052f68706a9fa36b688b26d5f8a8ec55b1
Ross Lew <rgglewerenz@gmaill.com>
2023-03-23 15:42:54 -0400

    Moved the password validation into the DTO

--- DTO/CreateUserTransferModal.cs
+++ DTO/CreateUserTransferModal.cs
@@ -10,17 +10,19 @@ namespace DTO
     public class CreateUserTransferModal
     {
         [Required]
-        [StringLength(20, ErrorMessage = "Username must be shorter then 20 characters")]
+        [StringLength(20, MinimumLength =5, ErrorMessage = "Username must be shorter then 20 characters, but greater than 5")]
         public string UserName { get; set; } = "";
         [Required]
         [EmailAddress]
         public string Email { get; set; } = "";
 
         [Required]
+        [Range(18,int.MaxValue, ErrorMessage = "You must be 18 or older to sign up")]
         public int Age { get; set; }
 
         [Required]
-        [StringLength(30, ErrorMessage = "Password must be shorter then 30 characters")]
+        [PasswordValidation]
+        [StringLength(30, MinimumLength = 10, ErrorMessage = "Password must be shorter then 30 characters, but greater then 10 characters")]
         public string Password { get; set; }
 
     };
---
+++ DTO/PasswordValidation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    sealed public class PasswordValidation : ValidationAttribute
+    {
+        public PasswordValidation()
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            return CheckPassComplexity((String)value);
+        }
+
+        private bool CheckPassComplexity(string password)
+        {
+            if (!password.Any(char.IsDigit))
+            {
+                ErrorMessage = "Password must conatin a number";
+                return false;
+            }
+            if (!password.Any(char.IsLetter))
+            {
+                ErrorMessage = "Password must conatin a letter";
+                return false;
+            }
+            if (!password.Any(char.IsPunctuation) && !password.Any(char.IsSymbol))
+            {
+                ErrorMessage = "Password must conatin a special character";
+                return false;
+            }
+            return true;
+        }
+    }
+}
